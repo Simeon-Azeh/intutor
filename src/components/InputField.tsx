21e2a445000361eff624790d984e91fcc16afda8
Simeon-Azeh <144020513+Simeon-Azeh@users.noreply.github.com>
2024-09-26 20:57:16 +0200

    update eslint

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,16 +1,16 @@
 import { FieldError, UseFormRegister } from "react-hook-form";
 
-type InputFieldProps = {
+type InputFieldProps<TFormValues> = {
   label: string;
   type?: string;
-  register: UseFormRegister<any>; // Update the type of register
-  name: string;
+  register: UseFormRegister<TFormValues>; // Make register type-safe
+  name: keyof TFormValues; // Use keyof to reference form fields correctly
   defaultValue?: string;
   error?: FieldError;
   inputProps?: React.InputHTMLAttributes<HTMLInputElement>;
 };
 
-const InputField = ({
+const InputField = <TFormValues,>({
   label,
   type = "text",
   register,
@@ -18,7 +18,7 @@ const InputField = ({
   defaultValue,
   error,
   inputProps,
-}: InputFieldProps) => {
+}: InputFieldProps<TFormValues>) => {
   return (
     <div className="flex flex-col gap-2 w-full md:w-1/4">
       <label className="text-xs text-gray-500">{label}</label>
