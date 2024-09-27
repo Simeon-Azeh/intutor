6d6777bd5930ab12afe7fe2993748ea8c24712f0
Simeon-Azeh <144020513+Simeon-Azeh@users.noreply.github.com>
2024-09-27 08:41:47 +0200

    done with tables

--- src/app/(dashboard)/list/assignments/page.tsx
+++ src/app/(dashboard)/list/assignments/page.tsx
@@ -2,14 +2,18 @@ import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { lessonsData, role } from "@/lib/data";
+import {
+  assignmentsData,
+  role,
+} from "@/lib/data";
 import Image from "next/image";
 
-type Lesson = {
+type Assignment = {
   id: number;
   subject: string;
   class: string;
   teacher: string;
+  dueDate: string;
 };
 
 const columns = [
@@ -27,26 +31,32 @@ const columns = [
     className: "hidden md:table-cell",
   },
   {
+    header: "Due Date",
+    accessor: "dueDate",
+    className: "hidden md:table-cell",
+  },
+  {
     header: "Actions",
     accessor: "action",
   },
 ];
 
-const LessonListPage = () => {
-  const renderRow = (item: Lesson) => (
+const AssignmentListPage = () => {
+  const renderRow = (item: Assignment) => (
     <tr
       key={item.id}
-      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-orange-50"
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
     >
       <td className="flex items-center gap-4 p-4">{item.subject}</td>
       <td>{item.class}</td>
       <td className="hidden md:table-cell">{item.teacher}</td>
+      <td className="hidden md:table-cell">{item.dueDate}</td>
       <td>
         <div className="flex items-center gap-2">
-          {role === "admin" && (
+          {role === "admin" || role === "teacher" && (
             <>
-              <FormModal table="lesson" type="update" data={item} />
-              <FormModal table="lesson" type="delete" id={item.id} />
+              <FormModal table="assignment" type="update" data={item} />
+              <FormModal table="assignment" type="delete" id={item.id} />
             </>
           )}
         </div>
@@ -55,35 +65,31 @@ const LessonListPage = () => {
   );
 
   return (
-    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-2">
+    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Lessons</h1>
+        <h1 className="hidden md:block text-lg font-semibold">
+          All Assignments
+        </h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
-            <button
-            type="button"
-            title="button"
-             className="w-8 h-8 flex items-center justify-center rounded-full bg-orange-50">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/filter.png" alt="" width={14} height={14} />
             </button>
-            <button
-             type="button"
-            title="button"
-             className="w-8 h-8 flex items-center justify-center rounded-full bg-orange-50">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
-            {role === "admin" && <FormModal table="lesson" type="create" />}
+            {role === "admin" || role === "teacher" && <FormModal table="assignment" type="create" />}
           </div>
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={lessonsData} />
+      <Table columns={columns} renderRow={renderRow} data={assignmentsData} />
       {/* PAGINATION */}
       <Pagination />
     </div>
   );
 };
 
-export default LessonListPage;
+export default AssignmentListPage;
