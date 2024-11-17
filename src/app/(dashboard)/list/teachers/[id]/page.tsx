2764a672388003c41f32c6bbcc37802536fcaa7b
Simeon-Azeh <144020513+Simeon-Azeh@users.noreply.github.com>
2024-11-17 12:00:07 +0100

    completed some more teacher details

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -5,6 +5,7 @@ import Performance from "@/components/Performance";
 import { role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
+import { School, BookCopy, Banknote, Calendar, MapPin, BookOpen } from 'lucide-react';
 
 const SingleTeacherPage = () => {
   return (
@@ -14,19 +15,19 @@ const SingleTeacherPage = () => {
         {/* TOP */}
         <div className="flex flex-col lg:flex-row gap-4">
           {/* USER INFO CARD */}
-          <div className="bg-lamaSky py-6 px-4 rounded-md flex-1 flex gap-4">
-            <div className="w-1/3">
+          <div className="bg-white py-4 px-6 ">
+          <div className="flex justify-center">
               <Image
-                src="https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200"
+                src="/avatar.png"
                 alt=""
                 width={100}
                 height={100}
-                className=" rounded-full object-cover"
+                className="rounded-[200px] object-cover mb-2"
               />
             </div>
-            <div className="w-2/3 flex flex-col justify-between gap-4">
+            <div className="flex flex-col justify-between gap-4">
               <div className="flex items-center gap-4">
-                <h1 className="text-xl font-semibold">Leonard Snyder</h1>
+                <h1 className="text-xl font-semibold text-[#1a1a1a]">Agbor Takeng</h1>
                 {role === "admin" && <FormModal
                   table="teacher"
                   type="update"
@@ -37,12 +38,12 @@ const SingleTeacherPage = () => {
                     password: "password",
                     firstName: "Dean",
                     lastName: "Guerrero",
-                    phone: "+1 234 567 89",
+                    phone: "+237 654 693 75",
                     address: "1234 Main St, Anytown, USA",
                     bloodType: "A+",
                     dateOfBirth: "2000-01-01",
                     sex: "male",
-                    img: "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200",
+                    img: "/avatar.png",
                   }}
                 />}
               </div>
@@ -50,80 +51,75 @@ const SingleTeacherPage = () => {
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
               </p>
               <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
-                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
-                  <Image src="/blood.png" alt="" width={14} height={14} />
-                  <span>A+</span>
+                <div className="w-full  flex items-center gap-2">
+                <div className="flex items-center gap-4">
+                  <div className="flex items-center gap-2">
+                  <School size={16} />
+                  <span>2B, 1B, 3C, 2A, EN5, 6A</span>
+                  |
+                  </div>
+                  <div  className="flex items-center gap-2">
+                  <BookCopy size={16} />
+                  <span>Eng Phy, Maths203, CHEM201</span>
+                  </div>
                 </div>
-                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+               
+                </div>
+                <div className="w-full  flex items-center gap-2">
+                <div className="flex items-center gap-4">
+                  <div className="flex items-center gap-2">
                   <Image src="/date.png" alt="" width={14} height={14} />
-                  <span>January 2025</span>
+                  <span>Aug 2024</span>
+                  |
+                  </div>
+                  <div  className="flex items-center gap-2">
+                  <Banknote size={16} />
+                  <span>120,000 xaf</span>
+                  </div>
                 </div>
-                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                
+               
+                </div>
+                <div className="flex items-center gap-4">
+                  <div className="flex items-center gap-2">
                   <Image src="/mail.png" alt="" width={14} height={14} />
                   <span>user@gmail.com</span>
-                </div>
-                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  |
+                  </div>
+                  <div  className="flex items-center gap-2">
                   <Image src="/phone.png" alt="" width={14} height={14} />
-                  <span>+1 234 567</span>
+                  <span>+237 654 693 75</span>
+                  </div>
                 </div>
+             
+          
               </div>
             </div>
           </div>
           {/* SMALL CARDS */}
           <div className="flex-1 flex gap-4 justify-between flex-wrap">
             {/* CARD */}
-            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
-              <Image
-                src="/singleAttendance.png"
-                alt=""
-                width={24}
-                height={24}
-                className="w-6 h-6"
-              />
-              <div className="">
-                <h1 className="text-xl font-semibold">90%</h1>
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-full 2xl:w-[48%]">
+              <Calendar size={24} className="text-[#018abd]" />
+              <div>
+                <h1 className="text-xl font-semibold text-[#018abd]">90%</h1>
                 <span className="text-sm text-gray-400">Attendance</span>
               </div>
             </div>
+           
             {/* CARD */}
-            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
-              <Image
-                src="/singleBranch.png"
-                alt=""
-                width={24}
-                height={24}
-                className="w-6 h-6"
-              />
-              <div className="">
-                <h1 className="text-xl font-semibold">2</h1>
-                <span className="text-sm text-gray-400">Branches</span>
-              </div>
-            </div>
-            {/* CARD */}
-            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
-              <Image
-                src="/singleLesson.png"
-                alt=""
-                width={24}
-                height={24}
-                className="w-6 h-6"
-              />
-              <div className="">
-                <h1 className="text-xl font-semibold">6</h1>
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-full 2xl:w-[48%]">
+              <BookOpen size={24} className="text-[#018abd]" />
+              <div>
+                <h1 className="text-xl font-semibold text-[#018abd]">6</h1>
                 <span className="text-sm text-gray-400">Lessons</span>
               </div>
             </div>
             {/* CARD */}
-            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
-              <Image
-                src="/singleClass.png"
-                alt=""
-                width={24}
-                height={24}
-                className="w-6 h-6"
-              />
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-full 2xl:w-[48%]">
+            <School size={24} className="text-[#018abd]" />
               <div className="">
-                <h1 className="text-xl font-semibold">6</h1>
+                <h1 className="text-xl font-semibold text-[#018abd] ">6</h1>
                 <span className="text-sm text-gray-400">Classes</span>
               </div>
             </div>
@@ -131,7 +127,7 @@ const SingleTeacherPage = () => {
         </div>
         {/* BOTTOM */}
         <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
-          <h1>Teacher&apos;s Schedule</h1>
+          <h1 className="font-medium text-lg">Teacher&apos;s Schedule</h1>
           <BigCalendar />
         </div>
       </div>
